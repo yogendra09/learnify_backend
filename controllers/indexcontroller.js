5985c4e552e6cba648868f5cfdc9e12e5f600520
yogendra singh <91890064+yogendra09@users.noreply.github.com>
2024-12-14 21:41:33 +0530

    Update indexcontroller.js

--- controllers/indexcontroller.js
+++ controllers/indexcontroller.js
@@ -13,7 +13,7 @@ exports.homepage = catchAsyncErrors(async (req, res, next) => {
 exports.currentuser = catchAsyncErrors(async (req, res, next) => {
     console.log(req.headers.Authorization)
     let student = await studentModel.findById(req.id).exec()
-    res.json({ student,token:req.body.headers.Authorization })
+    res.json({ student,token:req.headers.Authorization })
 })
 
 
